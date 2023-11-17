4921058892b4542d01d336bd194fef9cb9fec92b
Yohei Yasukawa <yohei@yasslab.jp>
2023-11-17 13:11:13 +0900

    Add script to update dojo stats in GitHub profile

---
+++ update_profile.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'json'
+
+BASE_URL   = 'https://coderdojo.jp'
+DOJO_STATS = JSON.parse Net::HTTP.get(URI.parse "#{BASE_URL}/stats.json"), symbolize_names: true
+
+profile = File.read('profile/README.md')
+profile.sub!(/日本全国(\d+)/, "日本全国#{DOJO_STATS[:active_dojos]}")
+
+File.open('profile/README.md', 'w') do |file|
+  file.write(profile)
+end
