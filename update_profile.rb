ea002ae0e8fb0e1adfdb509ffe1b20faad4c1f54
Yohei Yasukawa <yohei@yasslab.jp>
2024-04-22 20:59:14 +0900

    Add debug print for DOJO_STATS

--- update_profile.rb
+++ update_profile.rb
@@ -6,6 +6,7 @@ require 'json'
 BASE_URL   = 'https://coderdojo.jp'
 DOJO_STATS = JSON.parse Net::HTTP.get(URI.parse "#{BASE_URL}/stats.json"), symbolize_names: true
 
+puts "DOJO_STATS: " + DOJO_STATS[:active_dojos].to_s
 profile = File.read('profile/README.md')
 profile.sub!(/日本全国(\d+)/, "日本全国#{DOJO_STATS[:active_dojos]}")
 
