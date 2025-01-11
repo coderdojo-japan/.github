9355ec1dcdc0ef05efab4505f9c7a87adfec39e2
Yohei Yasukawa <yohei@yasslab.jp>
2025-01-11 21:49:16 +0900

    Update update_profile.rb

--- update_profile.rb
+++ update_profile.rb
@@ -8,7 +8,7 @@ DOJO_STATS = JSON.parse Net::HTTP.get(URI.parse "#{BASE_URL}/stats.json"), symbo
 
 puts "DOJO_STATS: " + DOJO_STATS[:active_dojos].to_s
 profile = File.read('profile/README.md')
-profile.sub!(/日本全国(\d+)/, "日本全国#{DOJO_STATS[:active_dojos]}")
+profile.sub!(/日本には(\d+)/, "日本には#{DOJO_STATS[:active_dojos]}")
 
 File.open('profile/README.md', 'w') do |file|
   file.write(profile)
